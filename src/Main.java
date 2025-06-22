3b8aed0094aa0c8939bb877e81ba3fe12ea7698f
Abdullah Amir <abdullahamir.online@gmail.com>
2025-06-22 12:47:12 +0500

    Add show task by Kamlesh Kumar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Welcome to Task Manager CLI");
+        showTasks();
+
     }
+
+    public static void showTasks() {
+        System.out.println("Showing tasks...");
+    }
+
 }
+
+
