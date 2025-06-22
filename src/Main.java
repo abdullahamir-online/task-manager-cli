275730ece3616ea8c24f07f6cf1e1896ef717f68
Abdullah Amir <abdullahamir.online@gmail.com>
2025-06-22 14:59:52 +0500

    Add deleteTask by Abdul Tayab

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Welcome to Task Manager CLI");
+        deleteTask();
     }
+
+    public static void deleteTask() {
+        System.out.println("Task deleted.");
+    }
+
 }
+
